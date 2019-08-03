8a6736012a3e3be0b48bf75a58ae12a8322e85ae
Kandhasamy <manivannank@vcs.privad.net>
2019-08-03 16:43:00 +0530

    updated

--- src/app/bookappointment/bookappointment.page.ts
+++ src/app/bookappointment/bookappointment.page.ts
@@ -10,6 +10,28 @@ export class BookappointmentPage implements OnInit {
 
   constructor( public toastController :ToastController ) { }
 
+
+  sessionList: Array<any> = [
+    { 
+      name: 'Morning', 
+      timeList : [
+        { name : 'Morning', value : 1 }
+      ] 
+    },
+    { name: 'Evening', 
+      timeList : [
+        { name : '7:30 pm', value : 2 },
+        { name : '8:00 pm', value : 3 },
+        { name : '8:30 pm', value : 4 },
+        { name : '9:00 pm', value : 5 }
+      ] 
+    },
+  ];
+  time: Array<any>;
+  changeTime(count) {
+    this.time = this.sessionList.find(con => con.name == count).time;
+  }
+
   ngOnInit() {
   }
 
